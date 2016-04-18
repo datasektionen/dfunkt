7b0f7b11edeb6f625bb0fb98f71cc05099f1d80b
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2016-04-18 15:18:18 +0200

    Error handling in mandates
    Now the server will not add a new mandate if one of the fields is missing.

--- routes/mandates.js
+++ routes/mandates.js
@@ -1,25 +1,46 @@
 var models  = require('../models');
 var express = require('express');
-var debug = require('debug')('Mandaties');
+var debug = require('debug')('mandates');
 var router  = express.Router();
 
+function validMandateRequest(body) {
+  return body.username && body.username !== "" &&
+         body.roleId &&
+         body.start &&
+         body.end;
+  // TODO: Check if end is after start
+}
+
 router.post('/create', function(req, res) {
-  debug("Searching for" + JSON.stringify(req.body.username));
-  models.User.findOne({
+  debug("Request body: " + JSON.stringify(req.body));
+  debug("Searching for user " + JSON.stringify(req.body.username));
+  if (validMandateRequest(req.body)) {
+    models.User.findOne({
       where: {
-          name:req.body.username
+        name:req.body.username
       }
-  }).then(function (user) {
-      debug("creating with user" + JSON.stringify(user));
-      models.Mandate.create({
-        start: req.body.start,
-        end: req.body.end,
-        UserId: user.id,
-        RoleId: req.body.roleId,
-      }).then(function() {
+    }).then(function (user) {
+      if (user) {
+        debug("creating with user" + JSON.stringify(user));
+        models.Mandate.create({
+          start: req.body.start,
+          end: req.body.end,
+          UserId: user.id,
+          RoleId: req.body.roleId,
+        }).then(function() {
+          res.redirect('/');
+        });
+      } else {
+        debug("Could not find a user for request: " + JSON.stringify(req.body));
+        // TODO: Add an error page informing user that the request failed
         res.redirect('/');
-      });
-  });
+      }
+    });
+  } else {
+    debug("Programmer error, missing fields, invalid request: " + JSON.stringify(req.body));
+    debug("Did js not stop you?");
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
