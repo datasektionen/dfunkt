113023a9ebbd77c40ae0322fb52a277d1f26281f
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2016-04-18 12:51:51 +0200

    SUbmitting mandates by user name works.
    No need to enter ID, just input the name and the system will find the appropriate user. We can copy the same system for roles, and we also need to add dropdowns that are sent along.
    
    ALso, any spelling mistake will result in failure, so we need to add along a dropdown (or select) to minimize user error. We need to:
    
     * Add dropdown/select
     * Handle failure
     * Use same system for roles
     * See if we can compare case-insensitively
     * Think about name clashes?

--- routes/mandates.js
+++ routes/mandates.js
@@ -1,15 +1,23 @@
 var models  = require('../models');
 var express = require('express');
+var debug = require('debug')('Mandaties');
 var router  = express.Router();
 
 router.post('/create', function(req, res) {
-  models.Mandate.create({
-    start: req.body.start,
-    end: req.body.end,
-    UserId: req.body.userId,
-    RoleId: req.body.roleId,
-  }).then(function() {
-    res.redirect('/');
+  models.User.findOne({
+      where: {
+          name:req.body.username
+      }
+  }).then(function (user) {
+      debug("creating with user" + JSON.stringify(user));
+      models.Mandate.create({
+        start: req.body.start,
+        end: req.body.end,
+        UserId: user.id,
+        RoleId: req.body.roleId,
+      }).then(function() {
+        res.redirect('/');
+      });
   });
 });
 
