c16af6d0f7787d48101bde1cd97b4a59b8bbbfac
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2016-04-18 17:06:59 +0200

    Role dropdown done
    Making better use of promises with `all()` and some other goodies.

--- routes/mandates.js
+++ routes/mandates.js
@@ -3,42 +3,64 @@ var express = require('express');
 var debug = require('debug')('mandates');
 var router  = express.Router();
 
-function validMandateRequest(body) {
+function validRequest(body) {
   return body.username && body.username !== "" &&
-         body.roleId &&
+         body.rolename && body.rolename !== "" &&
          body.start &&
          body.end &&
          new Date(body.start) <= new Date(body.end);
 }
 
+function makeSureNoneNull(values) {
+  for (var i = 0; i < values.length; i++) {
+    var value = values[i];
+    if (value === null) {
+      return Promise.reject(Error("Value #" + i + " was null."));
+    }
+  }
+  return Promise.resolve(values);
+};
+
 router.post('/create', function(req, res) {
   debug("Request body: " + JSON.stringify(req.body));
   debug("Searching for user " + JSON.stringify(req.body.username));
-  if (validMandateRequest(req.body)) {
-    models.User.findOne({
-      where: {
-        name:req.body.username
-      }
-    }).then(function (user) {
-      if (user) {
-        debug("creating with user" + JSON.stringify(user));
-        models.Mandate.create({
-          start: req.body.start,
-          end: req.body.end,
-          UserId: user.id,
-          RoleId: req.body.roleId,
-        }).then(function() {
-          res.redirect('/');
-        });
-      } else {
-        debug("Could not find a user for request: " + JSON.stringify(req.body));
-        // TODO: Add an error page informing user that the request failed
+
+  if (validRequest(req.body)) {
+    Promise.all([
+      models.User.findOne({
+        where: {
+          name:req.body.username
+        }
+      }),
+      models.Role.findOne({
+        where: {
+          name:req.body.rolename
+        }
+      }),
+    ]).then( makeSureNoneNull )
+    .then(function (results) {
+      var user = results[0];
+      var role = results[1];
+
+      debug("creating with user" + JSON.stringify(user));
+      debug("creating with role" + JSON.stringify(role));
+
+      models.Mandate.create({
+        start: req.body.start,
+        end: req.body.end,
+        UserId: user.id,
+        RoleId: role.id,
+      }).then(function() {
         res.redirect('/');
-      }
+      });
+    }, function(reason) {
+      debug("Could not find one of the needed fields, reason: ", reason);
+      debug("Request body: " + JSON.stringify(req.body));
+      res.redirect('/');
     });
   } else {
-    debug("Programmer error, missing fields, invalid request: " + JSON.stringify(req.body));
-    debug("Did js not stop you?");
+    debug("Invalid request: " + JSON.stringify(req.body));
+    // TODO: Add an error page informing user that the request failed
     res.redirect('/');
   }
 });
--- routes/roles.js
+++ routes/roles.js
@@ -2,14 +2,25 @@ var models  = require('../models');
 var express = require('express');
 var router  = express.Router();
 
+function validRequest(body) {
+  return body.name && body.name !== "" &&
+         body.description && body.description !== "" &&
+         body.email && body.email !== ""
+}
+
 router.post('/create', function(req, res) {
-  models.Role.create({
-    name: req.body.name,
-    email: req.body.email,
-    description: req.body.description,
-  }).then(function() {
+  if (validRequest(req.body)) {
+    models.Role.create({
+      name: req.body.name,
+      email: req.body.email,
+      description: req.body.description,
+    }).then(function() {
+      res.redirect('/');
+    });
+  } else {
+    debug("Invalid request: " + JSON.stringify(req.body));
     res.redirect('/');
-  });
+  }
 });
 
 module.exports = router;
