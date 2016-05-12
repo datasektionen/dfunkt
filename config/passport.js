c290b2d3367a75f624b961d4db8408e4884f0475
Jesper Simonsson <simonsson.jesper@gmail.com>
2016-05-12 18:09:14 +0200

    now updates/adds a user when they login

--- config/passport.js
+++ config/passport.js
@@ -3,7 +3,19 @@ var models  = require('../models');
 module.exports = function(passport) {
 
     passport.serializeUser(function(user, done) {
-        //user.user is the username of user, for example jsimo.
+        //We rely on the kthid to *never* change.
+        models.User.findOne({where: {kthid:user.user}}).then(function(dbuser) {
+        	if(!dbuser || !dbuser.ugkthid) {
+        		//If user does not exist or has not been fully create it.
+        		models.User.create({
+        			first_name: user.first_name,
+        			last_name: user.last_name,
+        			email: user.email,
+        			kthid: user.user,
+        			ugkthid: user.ugkthid,
+        		});
+        	}
+        });
         done(null, user.user);
     });
 
