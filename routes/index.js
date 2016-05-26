433d2b971ce4bf3cc2322537acecbcaccb7381f0
Jesper Simonsson <simonsson.jesper@gmail.com>
2016-05-26 20:56:00 +0200

    better looking views

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,22 @@ var models  = require('../models');
 var express = require('express');
 var router  = express.Router();
 var helpers = require('./helpers');
+var moment = require('moment');
 
 router.get('/', function(req, res) {
+  var now = new moment().format('YYYY-MM-DD');
   Promise.all([
     models.User.findAll({}),
     models.Role.findAll({include: [{model: models.Group, as: "Group"}]}),
-    models.Mandate.findAll({include: [{model: models.User, as: "User"},
-                                      {model: models.Role, as: "Role"}]}),
+    models.Mandate.findAll({
+      include: [{all: true}],
+      where: {start: {$lte: now}, end: {$gte: now}},
+      order: '"Role.GroupId", "Role.title"'
+    }),
     helpers.isadmin(req.user),
-    models.Group.findAll({}),
+    models.Group.findAll({
+      order: 'id'
+    }),
   ]).then(function(results) {
     var users = results[0];
     var roles = results[1];
@@ -32,6 +39,67 @@ router.get('/', function(req, res) {
     res.send('error');
   });
 });
+
+router.get('/user/:kthid', function(req, res) {
+  models.User.findOne({where: {kthid:req.params.kthid}}).then(function(user) {
+    Promise.all([
+      models.Mandate.findAll({
+        include: [{all: true}],
+        where: {UserId: user.id},
+        order: 'start DESC'
+      }),
+      helpers.isadmin(req.user),
+      models.Group.findAll({
+        order: 'id'
+      })
+    ]).then(function(results) {
+      var mandates = results[0];
+      var isadmin = results[1];
+      var groups = results[2];
+      res.render('user', {
+        user: req.user,
+        isadmin: isadmin,
+        mandates: mandates,
+        groups: groups,
+      });
+    }).catch(function(e) {
+      console.log(e);
+      res.status(403);
+      res.send('error');
+    });
+  });
+}); 
+
+router.get('/position/:ident', function(req, res) {
+  models.Role.findOne({where: {identifier:req.params.ident}}).then(function(role) {
+    Promise.all([
+      models.Mandate.findAll({
+        include: [{all: true}],
+        where: {RoleId: role.id},
+        order: 'start DESC'
+      }),
+      helpers.isadmin(req.user),
+      models.Group.findAll({
+        order: 'id'
+      })
+    ]).then(function(results) {
+      var mandates = results[0];
+      var isadmin = results[1];
+      var groups = results[2];
+      res.render('user', {
+        user: req.user,
+        isadmin: isadmin,
+        mandates: mandates,
+        groups: groups,
+      });
+    }).catch(function(e) {
+      console.log(e);
+      res.status(403);
+      res.send('error');
+    });
+  });
+}); 
+
 
 router.get('/admin', helpers.requireadmin, function(req, res) {
   Promise.all([
