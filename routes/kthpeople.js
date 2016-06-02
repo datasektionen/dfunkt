ce633dd52ae5d206de41c029f707ad01b35944f3
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2016-06-02 18:06:15 +0200

    Polished serach up. Responsive and shit.
    Gives some nice messages like "searching" and "no results". Also reduced
    it to one search bar because zfinger is good enough now.

--- routes/kthpeople.js
+++ routes/kthpeople.js
@@ -7,6 +7,8 @@ router.get('/', function(req, res) {
   res.render('kthsearch');
 });
 
+// TODO: Given recent improvements in zfinger, this can safely be removed.
+// Unless it is being used somewhere else?
 router.get('/kthid', function(req, res) {
   res.redirect('/kthpeople/kthid/' + req.query.kthid);
 });
@@ -33,13 +35,6 @@ router.get('/kthid/:kthid', function(req, res) {
 router.get('/search', function(req, res) {
   res.redirect('/kthpeople/search/' + req.query.query);
 });
-/*
-router.get('/search/:query', function(req, res) {
-  var query = req.params.query;
-  var url = 'https://zfinger.datasektionen.se/users/' + encodeURIComponent(query);
-  res.redirect(url);
-});
-*/
 router.get('/search/:query', function(req, res) {
   var query = req.params.query;
   var url = 'https://zfinger.datasektionen.se/users/' + encodeURIComponent(query);
@@ -51,9 +46,6 @@ router.get('/search/:query', function(req, res) {
     });
     
     get_res.on('end', () => {
-      //renderParams = JSON.parse(recv_data);
-      //renderParams.query = query;
-      //res.render('kthresults', renderParams);
       res.send(recv_data);
     });
   });
