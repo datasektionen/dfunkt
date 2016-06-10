7aedb37fc6fe83ebceb82622631f61e9705458d7
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2016-06-10 22:23:17 +0200

    Solid, tested db logic
    Handling local/zfinger discrepancies gloriously, and even errors in our
    db that should never happen.
    
        This commit brought to you by Promises. They are fucking great
        for structuring the flow of your program if you just user them
        *properly*.
    
    Also, I got an autocomplete plugin for my vim. Not I can write long
    names without my fingers hurting!

--- routes/mandates.js
+++ routes/mandates.js
@@ -27,22 +27,6 @@ function makeSureNoneNull(values) {
   return Promise.resolve(values);
 };
 
-function findOrCreateUser(user) {
-  debug("GOind to find or create dis: " + user);
-  return models.User.findOrCreate({
-    where: {
-      first_name: user.first_name,
-      last_name: user.last_name,
-      kthid: user.kthid,
-      ugkthid: user.ugkthid,
-    },
-    defaults: {
-      admin: false, 
-    },
-  }).then(function (result) {
-    return result[0];
-  });
-}
 
 function findThisUser(kthid, ugkthid) {
   return models.User.findOne({
@@ -63,17 +47,108 @@ function findThisUser(kthid, ugkthid) {
   });
 }
 
+function getUsThisUser(kthid, ugkthid) {
+  return findExactUserLocally(kthid, ugkthid)
+    .catch( (e) => findKthidUserLocally(kthid)
+            .then((usr) => zfingerUpdateUser(ugkthid, usr)))
+    .catch( (e) => findUgkthidUserLocally(ugkthid)
+            .then((usr) => zfingerUpdateUser(ugkthid, usr)))
+    .catch( (e) => zfingerCreateUser(ugkthid));
+}
+
+function zfingerUpdateUser(ugkthid, userToUpdate) {
+  return zfinger.byUgkthid(ugkthid).then(function(zfingerUser) {
+    if (hasConflictingUgkthid(userToUpdate, zfingerUser.ugkthid)) {
+      var errMsg = 
+        "Can't update user w/ ugkthid: " + userToUpdate.ugkthid + 
+        "to match zfinger user w/ ugkthid: " + zfingerUser.ugkthid +
+        " (Different ugkthids. This should never happen! Our db must be corrupted).";
+      debug(errMsg);
+      console.log("IMPORTANT: " + errMsg); // Because this should never ever happen
+      return Promise.reject(errMsg);
+    } else {
+      userToUpdate.first_name = zfingerUser.first_name;
+      userToUpdate.last_name = zfingerUser.last_name;
+      userToUpdate.kthid = zfingerUser.kthid;
+      userToUpdate.ugkthid = zfingerUser.ugkthid;
+      userToUpdate.save();
+      return userToUpdate;
+    }
+  });
+}
+
+function zfingerCreateUser(ugkthid) {
+  return zfinger.byUgkthid(ugkthid)
+    .then( function(zfingerUser) { //TODO: arrow
+      return findOrCreateUser(zfingerUser);
+    });
+}
+
+function findOrCreateUser(user) {
+  debug("GOind to find or create dis: " + user);
+  return models.User.findOrCreate({
+    where: {
+      first_name: user.first_name,
+      last_name: user.last_name,
+      kthid: user.kthid,
+      ugkthid: user.ugkthid,
+    },
+    defaults: {
+      admin: false, 
+    },
+  }).then(function (result) {
+    return result[0];
+  });
+}
+
+function hasConflictingUgkthid(userToCheck, correctUgkthid) {
+  return userToCheck.ugkthid && userToCheck.ugkthid !== correctUgkthid;
+}
+
+function findExactUserLocally(kthid, ugkthid) {
+  return models.User.findOne({
+    where: {
+      kthid: kthid,
+      ugkthid: ugkthid,
+    }
+  }).then(function(maybeUser) {
+    return maybeUser
+      ? Promise.resolve(maybeUser)
+      : Promise.reject("User with kthid/ugkthid " + kthid + "/" + ugkthid + " was not found locally.");
+  });
+}
+
+function findKthidUserLocally(kthid) {
+  return models.User.findOne({
+    where: {
+      kthid: kthid,
+    }
+  }).then(function(maybeUser) {
+    return maybeUser
+      ? Promise.resolve(maybeUser)
+      : Promise.reject("User with kthid " + kthid + " was not found locally.");
+  });
+}
+
+function findUgkthidUserLocally(ugkthid) {
+  return models.User.findOne({
+    where: {
+      ugkthid: ugkthid,
+    }
+  }).then(function(maybeUser) {
+    return maybeUser
+      ? Promise.resolve(maybeUser)
+      : Promise.reject("User with ugkthid " + ugkthid + " was not found locally.");
+  });
+}
+
+
 router.post('/create', helpers.requireadmin, function(req, res) {
   debug("Request body: " + JSON.stringify(req.body));
 
   if (validRequest(req.body)) {
     Promise.all([
-      findThisUser(req.body.kthid, req.body.ugkthid) // TODO: add validation with ugkthid smh // Might have just done this
-        .catch(function(err){
-          debug("err findthisuser: " + err);
-          return zfinger.byUgkthid(req.body.ugkthid)
-            .then(findOrCreateUser);
-        }),
+      getUsThisUser(req.body.kthid, req.body.ugkthid), 
       models.Role.findOne({ where: {
         id:req.body.roleId
       }}),
