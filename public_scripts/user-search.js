8e79b58b4e875147021ca47acac5fef683ca3a14
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2016-06-12 15:09:46 +0200

    Aand we have validation
    Currently only for the select mandate form. Should add for the others.
    
    Also, an alert if probbly not the best thing to use, but as for now I'm
    lazy.

--- public_scripts/user-search.js
+++ public_scripts/user-search.js
@@ -7,8 +7,19 @@ ReactDOM.render(
 
 var form = document.getElementById('form-create-mandate');
 form.onsubmit = function(){
-  alert("qwe");
-  return false;
+  var elements = form.elements;
+  if (isNonemptyString(elements["kthid"].value) &&
+      isNonemptyString(elements["ugkthid"].value)) {
+    return true;
+  } else {
+    alert("Vänligen välj en användare till mandatet.");
+    return false;
+  }
 };
+
+function isNonemptyString(str) {
+  return typeof(str) == 'string' && str != '';
+}
+    
 
 
