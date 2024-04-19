91560a064bb274864b298081f373dace54fbd05f
Herman Karlsson <herkarlsson@gmail.com>
2024-04-19 23:27:33 +0200

    consistent tabbing

--- config/strategies/dauth.js
+++ config/strategies/dauth.js
@@ -1,43 +1,44 @@
 var passport = require('passport');
 var CustomStrategy = require('passport-custom').Strategy;
 var https = require('https');
+var http = require('https');
 var env = require("../../util/env")
 
 
 function verify(token, callback) {
   var options = {
     host: env.login_api_url,
-    path: "/verify/" + token + ".json?api_key=" + env.login_key,
+    path: "/verify/" + token + "?api_key=" + env.login_key,
     method: "GET"
   };
 
   var requestCallback = function(res) {
-      var collectedData = "";
-      res.setEncoding("utf-8");
+    var collectedData = "";
+    res.setEncoding("utf-8");
 
-      res.on("data", function(data) {
-          collectedData += data;
-      });
+    res.on("data", function(data) {
+      collectedData += data;
+    });
 
-      res.on("end", function() {
-          if (collectedData) {
-            try {
-              var user = JSON.parse(collectedData);
-              callback(user);
-            } catch(e) {
-              callback(undefined);
-            }
-          } else {
-            callback(undefined);
-          }
-      });
-
-      res.on("error", function(err) {
+    res.on("end", function() {
+      if (collectedData) {
+        try {
+          var user = JSON.parse(collectedData);
+          callback(user);
+        } catch(e) {
           callback(undefined);
-      });
-    };
-    var request = https.request(options, requestCallback);
-    request.end();
+        }
+      } else {
+        callback(undefined);
+      }
+    });
+
+    res.on("error", function(err) {
+      callback(undefined);
+    });
+  };
+  var request = http.request(options, requestCallback);
+  request.end();
 }
 
 
