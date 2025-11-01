34189f110667411219453db190f16c71b7047194
Viktor Ekby <167118183+Poizon7@users.noreply.github.com>
2025-11-01 15:51:27 +0100

    migrate from login to sso

--- config/passport.js
+++ config/passport.js
@@ -1,39 +1,52 @@
-var models  = require('../models');
+var models = require('../models');
+var OpenIDConnectStrategy = require('passport-openidconnect');
 
 module.exports = function(passport) {
+    passport.use(new OpenIDConnectStrategy({
+        issuer: process.env['OIDC_PROVIDER'],
+        authorizationURL: process.env['OIDC_PROVIDER'] + '/authorize',
+        tokenURL: process.env['OIDC_PROVIDER'] + '/oauth/token',
+        userInfoURL: process.env['OIDC_PROVIDER'] + '/userinfo',
+        clientID: process.env['OIDC_ID'],
+        clientSecret: process.env['OIDC_SECRET'],
+        callbackURL: process.env['REDIRECT_URL'],
+        scope: ['openid', 'profile', 'email'],
+    },
+        function verify(issuer, profile, cb) {
+            //We rely on the kthid to *never* change.
+            models.User.findOne({ where: { kthid: profile.id } }).then(function(dbuser) {
+                if (!dbuser) {
+                    //If user does not exist create it.
+                    models.User.create({
+                        first_name: profile.givenName,
+                        last_name: profile.familyName,
+                        email: profile.emails[0].value,
+                        kthid: profile.id,
+                        ugkthid: "ug" + profile.id,
+                        admin: false,
+                    });
+                    console.log("adding user: " + profile.id);
+                } else if (!dbuser.ugkthid) { //Update user if they are loggin in for the first time.
+                    dbuser.update({
+                        first_name: profile.givenName,
+                        last_name: profile.familyName,
+                        email: profile.emails[0].value,
+                        kthid: profile.id,
+                        ugkthid: "ug" + profile.id,
+                        admin: false,
+                    });
+                    console.log("updating user: " + profile.id);
+                }
+            });
+            cb(null, profile.id);
+        }
+    ));
 
-  passport.serializeUser(function(user, done) {
-    //We rely on the kthid to *never* change.
-    models.User.findOne({where: {kthid:user.user}}).then(function(dbuser) {
-      if(!dbuser) {
-        //If user does not exist create it.
-        models.User.create({
-          first_name: user.first_name,
-          last_name: user.last_name,
-          email: user.email,
-          kthid: user.user,
-          ugkthid: user.ugkthid,
-          admin: false,
-        });
-        console.log("adding user: " + user.user);
-      } else if(!dbuser.ugkthid) { //Update user if they are loggin in for the first time.
-        dbuser.update({
-          first_name: user.first_name,
-          last_name: user.last_name,
-          email: user.email,
-          kthid: user.user,
-          ugkthid: user.ugkthid,
-          admin: false,
-        });
-        console.log("updating user: " + user.user);
-      }
+    passport.serializeUser(function(user, cb) {
+        cb(null, user)
     });
-    done(null, user.user);
-  });
 
-  passport.deserializeUser(function(obj, done) {
-    done(null, obj);
-  });
-
-  require('./strategies/dauth.js')();
-};
+    passport.deserializeUser(function(user, cb) {
+        cb(null, user);
+    });
+}
--- routes/login.js
+++ routes/login.js
@@ -1,17 +1,20 @@
-var passport = require('passport');
 var express = require('express');
 var debug = require('debug')('login');
 var router  = express.Router();
 
+var passport = require('passport');
 
 router.get('/test', function(req, res) {
 	res.send('test hello ' + req.user);
 });
 
-router.get('/dauth/:token', passport.authenticate('dauth', {
-  successRedirect: '/',
-  failureRedirect: '/',
-}));
+router.get('/oidc', passport.authenticate('openidconnect'));
+
+router.get('/callback',
+  passport.authenticate('openidconnect', { failureRedirect: '/login/oidc', failureMessage: true }),
+  function(req, res) {
+    res.redirect('/');
+  });
 
 router.get('/logout', function(req, res) {
 	req.logout();
